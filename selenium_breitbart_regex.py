9aac3b2c27b532f2293ba2fcc289ead68c2200e0
Phil Li <philipyli@yahoo.com>
2016-12-10 18:48:17 -0800

    taboola is done.  google ads not

--- selenium_breitbart_regex.py
+++ selenium_breitbart_regex.py
@@ -14,6 +14,7 @@ import time
 import random
 import csv
 import sys
+import os
 import re
 
 
@@ -21,7 +22,7 @@ def grabScreenshotAndAdURLs(publisherURL):
 	browser.get(publisherURL)
 
 	try:
-		print(browser.page_source)
+#		print(browser.page_source)
 
 		# Get the actual page dimensions using javascript
 		pageWidth  = browser.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
@@ -31,8 +32,8 @@ def grabScreenshotAndAdURLs(publisherURL):
 		# set screensize to maximum offered by site... but not to exceed client's physical screensize
 		browser.get_screenshot_as_file('publisherScreenshot.png') 
 
-#		grabDomainsOfGoogleAdvertisers(browser.page_source)
-#		grabBrandNamesOfTaboolaAdvertisers(browser.page_source)
+		grabDomainsOfGoogleAdvertisers()
+		grabBrandNamesOfTaboolaAdvertisers(browser.page_source)
 
 
 #<span class="static-text top-right"></span>
@@ -49,11 +50,42 @@ def grabScreenshotAndAdURLs(publisherURL):
 		return True
 
 
+## ADD REGEXs TO GRAB BRAND NAMES AND DOMAINS ##
 
-def grabDomainsOfGoogleAdvertisers(pageSource):
-	matches = re.findall(r'(adurl=)(http|https)(\:\/\/)([\w \d \s .]*)', pageSource)
-	for match in matches:
-		print(match[3]) 
+
+def grabDomainsOfGoogleAdvertisers():
+
+#	div_elems = browser.find_elements_by_xpath('//div[contains(@id, "google_ads_iframe")]')
+
+#	for div_elem in enumerate(div_elems):
+#	WebDriverWait wait = new WebDriverWait(driver, 30000)
+#	wait.until(ExpectedConditions.visibilityOfElementLocated((By.name("google_ads_iframe"))))
+
+#	ff = webdriver.Firefox()
+#	ff.get("http://somedomain/url_that_delays_loading")
+	try:
+		div_elems = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "adurl")))
+	except:
+		print("Unexpected error:", sys.exc_info()[0])
+		print(' not loaded successfully.')
+		return False
+
+	else:
+		print('blah')
+#		return True
+
+	print(div_elems)
+
+#	for div_elem in div_elems:
+#		browser.switch_to.frame(frame_reference=div_elem)
+#		print(browser.page_source)
+
+
+#		print(div_elem.get_attribute('outerHTML'))
+
+#	matches = re.findall(r'(adurl=)(http|https)(\:\/\/)([\w \d \s .]*)', pageSource)
+#	for match in matches:
+#		print(match[3]) 
 
 
 def grabBrandNamesOfTaboolaAdvertisers(pageSource):
@@ -68,14 +100,16 @@ def grabBrandNamesOfTaboolaAdvertisers(pageSource):
 ## starts here
 ##############
 
-path_to_chromedriver = '/Users/pli/Development/defund-breitbart/chromedriver' # change path as needed
+
+
+### CHANGE TO RELATIVE PATH ##
+
+path_to_chromedriver = os.path.dirname(os.path.abspath(__file__)) +'/chromedriver' # change path as needed
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 publisherURL = "http://www.breitbart.com/"
 
 for i in range(0,1):
-#latlongListFile = open('latlongs.txt')
-#latlongList = latlongListFile.read().splitlines()
 	grabScreenshotAndAdURLs(publisherURL)
 #	print('finished one!')
 	seconds = .1 + (random.random() * .1)
